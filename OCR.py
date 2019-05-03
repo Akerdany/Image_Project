4949bcb0a016bcea9f9166a67d950da6eb39935b
Omar Anas <rfomar1998@gmail.com>
2019-05-04 01:29:51 +0200

    C

--- OCR.py
+++ OCR.py
@@ -9,48 +9,38 @@ letters = dict({"[0.]":"0", "[1.]":"1","[2.]":"2", "[3.]":"3","[4.]":"4", "[5.]"
                  ,"[30.]":"U", "[31.]":"V","[32.]":"W", "[33.]":"X","[34.]":"Y", "[35.]":"Z"})
 img = cv2.imread('result.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-img3=cv2.imread('oly.png')
+img3=cv2.imread('shuffledWrong.png')
 gray3 = cv2.cvtColor(img3,cv2.COLOR_BGR2GRAY)
 
 # Now we split the image to 5000 cells, each 20x20 size
 cells = [np.hsplit(row,100) for row in np.vsplit(gray,180)]
 testCells = [np.hsplit(row,36) for row in np.vsplit(gray3,1)]
 
-# for row in np.vsplit(gray,180):
-#  for col in np.hsplit(row, 100):
-#   cv2.imshow("dd",col)
-#   cv2.waitKey(0)
-
-# Make it into a Numpy array. It size will be (50,100,20,20)
 x = np.array(cells)
 y = np.array(testCells)
-# Now we prepare train_data and test_data.
-train = x[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
-test = y.reshape(-1,400).astype(np.float32) # Size = (2500,400)
-# i=0;
-# for row in test:
-#  for col in row:   ## must remove . reshape .astype from test in order to appear pics
-#   cv2.imwrite("C:\\Users\\Omar Anas\\PycharmProjects\\ImageProcessing\\testCases\\"+str(i)+".png",col)
-#   i+=1
 
-# Create labels for train and test data
+train = x[:,:50].reshape(-1,400).astype(np.float32)
+# test= x[:,50:100].reshape(-1,400).astype(np.float32) ## If we gonna use the half of the data as test
+test = y.reshape(-1,400).astype(np.float32) ## if we gonna use sample of letters
+
 k = np.arange(36)
 train_labels = np.repeat(k,250)[:,np.newaxis]
-# test_labels = train_labels.copy()
-test_labels=np.repeat(k,36)
-# Initiate kNN, train the data, then test it with test data for k=1
+# test_labels = train_labels.copy() ## If we gonna use the half of the data as test
+test_labels=np.repeat(k,36) ## if we gonna use sample of letters
+
 knn = cv2.ml.KNearest_create()
 knn.train(train,cv2.ml.ROW_SAMPLE,train_labels)
 ret,result,neighbours,dist = knn.findNearest(test,k=5)
-#
+
 numStr=""
 for i in result:
  numStr+=letters[str(i)]+" "
 print (numStr)
+
 cv2.imshow("",img3)
 cv2.waitKey(0)
-# Now we check the accuracy of classification
-# For that, compare the result with test_labels and check which are wrong
+
+
 # matches = result==test_labels
 # correct = np.count_nonzero(matches)
 # accuracy = correct*100.0/result.size
