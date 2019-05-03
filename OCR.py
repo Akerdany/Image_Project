40cd205a453da8eeb32834442063f6b61c78c9a4
Omar Anas <rfomar1998@gmail.com>
2019-05-03 03:54:46 +0200

    some changes

--- OCR.py
+++ OCR.py
@@ -3,7 +3,11 @@ import cv2
 import pytesseract
 from PIL import Image
 
-#img = cv2.imread('result.png')
+letters = dict({"[0.]":"0", "[1.]":"1","[2.]":"2", "[3.]":"3","[4.]":"4", "[5.]":"5","[6.]":"6", "[7.]":"7","[8.]":"8", "[9.]":"9",
+    "[10.]":"A", "[11.]":"B","[12.]":"C", "[13.]":"D","[14.]":"E", "[15.]":"F","[16.]":"G", "[17.]":"H","[18.]":"I", "[19.]":"J"
+                 ,"[20.]":"K", "[21.]":"L","[22.]":"M", "[23.]":"N","[24.]":"O", "[25.]":"P","[26.]":"Q", "[27.]":"R","[28.]":"S", "[29.]":"T"
+                 ,"[30.]":"U", "[31.]":"V","[32.]":"W", "[33.]":"X","[34.]":"Y", "[35.]":"Z"})
+img = cv2.imread('result.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 img3=cv2.imread('oly.png')
 gray3 = cv2.cvtColor(img3,cv2.COLOR_BGR2GRAY)
@@ -39,15 +43,12 @@ knn = cv2.ml.KNearest_create()
 knn.train(train,cv2.ml.ROW_SAMPLE,train_labels)
 ret,result,neighbours,dist = knn.findNearest(test,k=5)
 #
-letters = dict({"0":"0", "1":"1","2":"2", "3":"3","4":"4", "5":"5","6":"6", "7":"7","8":"8", "9":"9",
-    "10":"A", "11":"B","12":"C", "13":"D","14":"E", "15":"F","16":"G", "17":"H","18":"I", "19":"J"
-                 ,"20":"K", "21":"L","22":"M", "23":"N","24":"O", "25":"P","26":"Q", "27":"R","28":"S", "29":"T"
-                 ,"30":"U", "31":"V","32":"W", "33":"X","34":"Y", "35":"Z"})
 numStr=""
 for i in result:
- numStr+=str(i)
-
+ numStr+=letters[str(i)]+" "
 print (numStr)
+cv2.imshow("",img3)
+cv2.waitKey(0)
 # Now we check the accuracy of classification
 # For that, compare the result with test_labels and check which are wrong
 # matches = result==test_labels
