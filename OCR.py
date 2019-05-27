520e331174f0bdb053fe53b5530a1fc8ff18dbff
Omar Anas <rfomar1998@gmail.com>
2019-05-27 12:50:02 +0200

    doc

--- OCR.py
+++ OCR.py
@@ -7,31 +7,30 @@ letters = dict({"[0.]":"0", "[1.]":"1","[2.]":"2", "[3.]":"3","[4.]":"4", "[5.]"
     "[10.]":"A", "[11.]":"B","[12.]":"C", "[13.]":"D","[14.]":"E", "[15.]":"F","[16.]":"G", "[17.]":"H","[18.]":"I", "[19.]":"J"
                  ,"[20.]":"K", "[21.]":"L","[22.]":"M", "[23.]":"N","[24.]":"O", "[25.]":"P","[26.]":"Q", "[27.]":"R","[28.]":"S",
                 "[29.]":"T" ,"[30.]":"U", "[31.]":"V","[32.]":"W", "[33.]":"X","[34.]":"Y", "[35.]":"Z"})
+
 img = cv2.imread('result.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 img3=cv2.imread('shuffledWrong.png')
 gray3 = cv2.cvtColor(img3,cv2.COLOR_BGR2GRAY)
 rows, cols=gray3.shape
-# Now we split the image to 5000 cells, each 20x20 size
+
+#SEGMENTATION
 cells = [np.hsplit(row,100) for row in np.vsplit(gray,180)]
 testCells = [np.hsplit(row,(cols/20)) for row in np.vsplit(gray3,(rows/20))]
 
 x = np.array(cells)
 y = np.array(testCells)
-train = x[:,:80].reshape(-1,400).astype(np.float32)
-test= x[:,80:100].reshape(-1,400).astype(np.float32)
+
+#Training & Testing
+train = x[:,:80].reshape(-1,400).astype(np.float32) # 80 for Training
+test= x[:,80:100].reshape(-1,400).astype(np.float32) # 20 for Testing
 testCase = y.reshape(-1,400).astype(np.float32)
 
-# for row in test:
-#  for col in row:   ## must remove . reshape .astype from test in order to appear pics
-#   print(col)
-  # cv2.imshow("g",col)
-  # cv2.waitKey(0)
-
+# Assigning Values to each Letter and Number.
 k = np.arange(36)
-train_labels = np.repeat(k,400)[:,np.newaxis]
-test_labels = np.repeat(k,100)[:,np.newaxis]
-
+train_labels = np.repeat(k,400)
+test_labels = np.repeat(k,100)
+print(test_labels)
 
 knn = cv2.ml.KNearest_create()
 knn.train(train,cv2.ml.ROW_SAMPLE,train_labels)
@@ -42,11 +41,8 @@ for i in result:
  numStr+=letters[str(i)]+" "
 print (numStr)
 
-# cv2.imshow("",img3)
-# cv2.waitKey(0)
-
 print ('\n Confusion Matrix: \n')
-confusion_matrix_output =confusion_matrix(test_labels, result)
+confusion_matrix_output = confusion_matrix(test_labels, result)
 print (end='  ')
 for i in letters:
     print (letters[str(i)] ,end=' ')
